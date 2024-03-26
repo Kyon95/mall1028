90284d54a1ea189ffc0541c5831fe07a18c08b9c
dydx2000 <dydx2000.py@gmail.com>
2024-03-26 16:07:04 +0800

    项目初始化

---
+++ src/main/java/com/geekaca/mall/Mall1028Application.java
@@ -0,0 +1,13 @@
+package com.geekaca.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall1028Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall1028Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/geekaca/mall/Mall1028ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekaca.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mall1028ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
