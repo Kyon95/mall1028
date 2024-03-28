be890b2b1a71898b08bdea30cfe22008f657d6d9
daguang <renchonglang_12345@user.noreply.gitee.com>
2024-03-28 14:39:50 +0800

    logback 文件路径引用配置文件的定义

--- src/main/java/com/geekaca/mall/Mall1028Application.java
+++ src/main/java/com/geekaca/mall/Mall1028Application.java
@@ -1,14 +1,16 @@
 package com.geekaca.mall;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 @MapperScan("com.geekaca.mall.mapper") // 扫描 Mapper 接口
 public class Mall1028Application {
 
     public static void main(String[] args) {
+        log.info("test 1028");
         SpringApplication.run(Mall1028Application.class, args);
     }
 
