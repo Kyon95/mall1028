b234e94ce19be1c56633b9c1fd4af84ee33bbffa
杨煊友 <dydx2000.py@gmail.com>
2024-04-06 13:25:18 +0800

    前台订单模块与分类模块，优化返回前端数据，返回VO类型。

---
+++ src/main/java/com/geekaca/mall/controller/vo/SecondLevelCategoryVO.java
@@ -0,0 +1,45 @@
+package com.geekaca.mall.utils;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.geekaca.mall.domain.GoodsCategory;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Data
+public class SecondLevelCategoryVOS {
+
+    private Long categoryId;
+
+    /**
+     * 分类级别(1-一级分类 2-二级分类 3-三级分类)
+     */
+    private Integer categoryLevel;
+
+    /**
+     * 父分类id
+     */
+    private Long parentId;
+
+    /**
+     * 分类名称
+     */
+    private String categoryName;
+
+    /**
+     * 排序值(字段越大越靠前)
+     */
+    private Integer categoryRank;
+
+
+
+
+
+
+
+
+//    private List<GoodsCategory> secondLevelCategoryVOS;
+    private List<GoodsCategory> thirdLevelCategoryVOS;
+}
