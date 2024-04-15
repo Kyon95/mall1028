7bb0d0349ed5844045bb0e41ef52d753a9cff2c8
Yangjiaxin <Kyon_Yjx@outlook.com>
2024-04-16 00:01:05 +0800

    1、实现放行商场首页 2、针对前台和后台进行分开校验token

--- src/main/java/com/geekaca/mall/config/TokenInterceptor.java
+++ src/main/java/com/geekaca/mall/config/TokenInterceptor.java
@@ -14,6 +14,8 @@ import redis.clients.jedis.JedisPool;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -27,25 +29,51 @@ public class TokenInterceptor implements HandlerInterceptor {
         if (HttpMethod.OPTIONS.toString().equals(request.getMethod())) {
             return true;
         }
+        //放行mall商场首页，不登入也能浏览首页
+        String requestUri = request.getRequestURI();
+        if (isExcludedPath(requestUri)) {
+            return true;
+        }
+
+        // 对后台管理接口进行拦截
+        if (requestUri.startsWith("/manage-api/v1")) {
+            return handleToken(request, "uid:admin:");
+        }
+
+        // 对前台管理接口进行拦截
+        if (requestUri.startsWith("/api/v1")) {
+            return handleToken(request, "uid:user:");
+        }
+
+        return true;
+    }
+
+    // 判断请求路径是否在排除列表中
+    private boolean isExcludedPath(String requestUri) {
+        // 定义不需要拦截的路径列表
+        List<String> excludedPaths = Arrays.asList("/#/home");
+        // 检查请求的路径是否在排除列表中
+        return excludedPaths.stream().anyMatch(requestUri::endsWith);
+    }
+
+
+    private boolean handleToken(HttpServletRequest request, String redisKeyPrefix) {
+        // 实现 Token 的验证逻辑
         String token = request.getHeader("Token");
         if (token == null || token.equals("")) {
             throw new NotLoginException(Constants.NO_LOGIN, "未携带token");
         }
         Map<String, Claim> stringClaimMap = JwtUtil.verifyToken(token);
         Claim idClaim = stringClaimMap.get("id");
-        /**
-         * 检验用户身份 把登录成功 的token 存入redis
-         */
         String uid = idClaim.asString();
         try(Jedis jedis = jedisPool.getResource();){
-            String redisAdminToken = jedis.get("uid:admin:" + uid);
-            String redisUserToken = jedis.get("uid:user:" + uid);
-
-            if (redisAdminToken == null && redisUserToken == null){
+            String redisToken = jedis.get(redisKeyPrefix + uid);
+            if (redisToken == null){
                 // 说明token过期，或者被删除了token
                 throw new NotLoginException(Constants.NO_LOGIN, "token无效或超时");
             }
         }
         return true;
     }
+
 }
